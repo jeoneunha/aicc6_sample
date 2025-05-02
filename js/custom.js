a7dc04c2fd95a7286ccf8d1cf2ee1490b1e42e66
jeoneunha <lubmin@naver.com>
2025-05-02 12:14:59 +0900

    v1

--- js/custom.js
+++ js/custom.js
@@ -1,20 +1,38 @@
 // menu toggle
-const menu = document.querySelector(".menu");
-const icon = document.querySelector(".menu i");
+const menu = document.querySelector('.menu');
+const icon = document.querySelector('.menu i');
+const nav = document.querySelector('.navi');
 
-menu.addEventListener("click", function(){
-    this.classList.toggle("active");
+menu.addEventListener('click', function () {
+  this.classList.toggle('active');
+  const navHeight = nav.scrollHeight;
 
-    // if(this.classList.contains("active")) {
-    //     // console.log("있다");
-    //     icon.setAttribute("class", "ri-close-line");
-    // } else {
-    //     // console.log("없다");
-    //     icon.setAttribute("class", "ri-menu-line");
-    // }
+  console.log(navHeight);
 
-    this.classList.contains("active") ? icon.setAttribute("class", "ri-close-line") : icon.setAttribute("class", "ri-menu-line") ;
+  if (this.classList.contains('active')) {
+    icon.setAttribute('class', 'ri-close-line');
+    nav.style.height = navHeight + 'px';
+  } else {
+    icon.setAttribute('class', 'ri-menu-line');
+    nav.style.height = 0;
+  }
 
-})
+  //   this.classList.contains('active')
+  //     ? icon.setAttribute('class', 'ri-close-line')
+  //     : icon.setAttribute('class', 'ri-menu-line');
+});
 
-console.log(menu);
+// Navigation Active
+const navs = document.querySelectorAll('.navi li');
+
+navs.forEach((nav, idx) => {
+  nav.addEventListener('click', function () {
+    navs.forEach((nav) => {
+      nav.classList.remove('active');
+    });
+
+    this.classList.add('active');
+  });
+});
+
+//console.log(navs);
